054c4904614cfa6a38cf860cf85cf46b38b17817
Sachin-MB <sachin.nagraj@mindbowser.com>
2025-07-15 12:49:27 +0530

    Update fhir-client-v0.1.12.js

--- example-smart-app/lib/js/fhir-client-v0.1.12.js
+++ example-smart-app/lib/js/fhir-client-v0.1.12.js
@@ -16920,9 +16920,9 @@ function urlParam(p, forceArray) {
   return result[0];
 }
 
-function stripTrailingSlash(var str) {
-    if(str.substring(-1) === '/') {
-        return str.substring(0, str.length - 1);
+function stripTrailingSlash(str) {
+    if(str.substr(-1) === '/') {
+        return str.substr(0, str.length - 1);
     }
     return str;
 }
